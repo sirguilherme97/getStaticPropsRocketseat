653c351a22a337c9afc8ae5acb857738aa1b8d73
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-07-08 10:49:40 -0300

    NextConfiguration, access

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://get-static-props-rocketseat.vercel.app/members/',
+  },
+
 }
 
 module.exports = nextConfig
