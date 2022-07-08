3fa170e78d90a9692af0c1740529af28e7468c2b
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-07-08 10:02:30 -0300

    agora vai again

--- src/pages/members/[login].tsx
+++ src/pages/members/[login].tsx
@@ -2,28 +2,28 @@ import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image"
 import Header from "../../componets/Header"
 
-export default function Member({ avatar_url, name, bio, company, followers, following }) {
+export default function Member({ user }) {
     return (
         <div>
             <Header />
             <div className="flex flex-col mt-12 justify-center items-center gap-4 p-4">
                 <div className="flex gap-4 items-center ">
                     <Image
-                        src={avatar_url}
-                        alt={name}
+                        src={user.avatar_url}
+                        alt={user.name}
                         width={96}
                         height={96}
                         className="rounded-full"
                     />
                     <div className="flex flex-col gap-4">
                         <div className="flex flex-col justify-center items-start">
-                            <p>{name}</p>
-                            <p>{bio}</p>
-                            <p>{company}</p>
+                            <p>{user.name}</p>
+                            <p>{user.bio}</p>
+                            <p>{user.company}</p>
                         </div>
                         <div className="flex gap-4 items-center ">
-                            <p>Seguidores: {followers}</p>
-                            <p>Seguindo: {following}</p>
+                            <p>Seguidores: {user.followers}</p>
+                            <p>Seguindo: {user.following}</p>
                         </div>
                     </div>
                 </div>
