d42403a6145662a86b465b99c404ff093e99513d
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-07-08 11:49:11 -0300

    Linking Members Buttom in Header, to Members Only

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
             <Link href="/blog">
                 <a className="font-medium underline underline-offset-2 hover:text-zinc-300 transition-colors text-2xl text-zinc-50 ">Blog</a>
             </Link>
-            <Link href="/members/diego3g">
+            <Link href="/members">
                 <a className="font-medium underline underline-offset-2 hover:text-zinc-300 transition-colors text-2xl text-zinc-50 ">Membros</a>
             </Link>
         </div>
